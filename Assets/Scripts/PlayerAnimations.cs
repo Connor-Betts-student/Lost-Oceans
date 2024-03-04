5c85f7e393ed42ac8a86c62406c10948574c02af
Connor Betts <112406724+Connor-Betts-student@users.noreply.github.com>
2024-03-04 10:40:29 -0500

    worked on animations

--- Assets/Scripts/PlayerAnimations.cs
+++ Assets/Scripts/PlayerAnimations.cs
@@ -18,15 +18,44 @@ public class WalkingButton : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.W))
         {
-            Player_Animations.SetBool("Bool", true);
+            Player_Animations.SetBool("BoolWalking", true);
         }
         if (Input.GetKey(KeyCode.W))
         {
-            Player_Animations.SetBool("Bool", true);
+            Player_Animations.SetBool("BoolWalking", true);
         }
         if (Input.GetKeyUp(KeyCode.W))
         {
-            Player_Animations.SetBool("Bool", false);
+            Player_Animations.SetBool("BoolWalking", false);
+        }
+    }
+}
+
+public class RunningButton : MonoBehaviour
+{
+
+    public Animator Player_Animations;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            Player_Animations.SetBool("BoolRunning", true);
+        }
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Player_Animations.SetBool("BoolRunning", true);
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            Player_Animations.SetBool("BoolRunning", false);
         }
     }
 }
