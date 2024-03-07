46cac80c697c4a777ef8ae36dfdff7763ffa28bc
Connor Betts <112406724+Connor-Betts-student@users.noreply.github.com>
2024-03-07 10:35:15 -0500

    added left and right animations

--- Assets/Scripts/PlayerAnimations.cs
+++ Assets/Scripts/PlayerAnimations.cs
@@ -14,35 +14,93 @@ public class RunningButton : MonoBehaviour
 
     }
 
+    bool AreKeysDown()
+    {
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.W))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+
         if (Input.GetKeyDown(KeyCode.W))
         {
-            Player_Animations.SetBool("BoolWalking", true);
+            Player_Animations.SetBool("BoolWalk", true);
         }
         if (Input.GetKey(KeyCode.W))
         {
-            Player_Animations.SetBool("BoolWalking", true);
+            Player_Animations.SetBool("BoolWalk", true);
         }
         if (Input.GetKeyUp(KeyCode.W))
         {
-            Player_Animations.SetBool("BoolWalking", false);
+            Player_Animations.SetBool("BoolWalk", false);
         }
 
-        if (Input.GetKeyDown(KeyCode.LeftShift))
+
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.W)) 
         {
-            Player_Animations.SetBool("BoolRunning", true);
+            Player_Animations.SetBool("BoolRun", true);
         }
-        if (Input.GetKey(KeyCode.LeftShift))
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.W))
         {
-            Player_Animations.SetBool("BoolRunning", true);
+            Player_Animations.SetBool("BoolRun", true);
         }
-        if (Input.GetKeyUp(KeyCode.LeftShift))
+        if (Input.GetKeyUp(KeyCode.LeftShift) || Input.GetKeyUp(KeyCode.W))
         {
-            Player_Animations.SetBool("BoolRunning", false);
+            Player_Animations.SetBool("BoolRun", false);
         }
 
+
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Player_Animations.SetBool("BoolLeft", true);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Player_Animations.SetBool("BoolLeft", true);
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            Player_Animations.SetBool("BoolLeft", false);
+        }
+
+
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Player_Animations.SetBool("BoolRight", true);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Player_Animations.SetBool("BoolRight", true);
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            Player_Animations.SetBool("BoolRight", false);
+        }
+
+
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Player_Animations.SetBool("BoolWalkBackward", true);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Player_Animations.SetBool("BoolWalkBackward", true);
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            Player_Animations.SetBool("BoolWalkBackward", false);
+        }
     }
 }
